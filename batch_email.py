8d8fd50475d37c464f00db1dd063c8a406a5caa8
subh77 <subhro77@outlook.com>
2021-05-08 17:42:22 +0530

    Importing dependencies

--- batch_email.py
+++ batch_email.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import numpy as np
 
 print(sys.version)
 print(sys.executable)
@@ -7,4 +8,3 @@ print(sys.version)
 
 df = pd.read_csv(r'/mnt/c/users/Clive/Desktop/new_devs_india.csv')
 print (df.head)
-
