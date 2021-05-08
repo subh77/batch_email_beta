17a52952d9cd2fe192a924fddfadeecc694d86f2
subh77 <subhro77@outlook.com>
2021-05-08 16:55:35 +0530

    Initial Code and env setup

---
+++ batch_email.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+print(sys.version)
+print(sys.executable)
+print(sys.version)
+
+df = pd.read_csv(r'/mnt/c/users/Clive/Desktop/new_devs_india.csv')
+# print (df.head)
+
